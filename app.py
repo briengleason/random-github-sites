23a5dc395180db371000720bb955f3ccaae1bb3e
briengleason <32398682+briengleason@users.noreply.github.com>
2020-12-27 18:37:46 -0500

    Add a basic index endpoint and update pages endpoint to be a POST request

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from query_github_sites import find_random_page
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Index Page'
 
-@app.route('/api/random_site')
+@app.route('/api/random_site', methods=['POST'])
 def get_random_site():
     return find_random_page()
 
