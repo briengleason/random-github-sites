abf3ef966be0c224429bde0578972bc9e1c00b02
briengleason <bgleason@deloitte.com>
2020-12-28 11:24:19 -0500

    allow CORS and require password for Flask app

--- application.py
+++ application.py
@@ -1,15 +1,34 @@
 from flask import Flask
 from flask_cors import CORS
 from query_github_sites import find_random_page
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
 
 application = Flask(__name__)
-CORS(application)
+
+cors = CORS(application, resources={r"/api/*": {"origins": "*"}})
+application.config['CORS_HEADERS'] = 'Content-Type'
+
+auth = HTTPBasicAuth()
+users = {
+    os.environ.get('API_USERNAME'): generate_password_hash(os.environ('API_PASSWORD'))
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 @application.route('/')
 def index():
     return 'Index Page'
 
 @application.route('/api/random_site', methods=['POST'])
+@auth.login_required
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_random_site():
     return find_random_page()
 
