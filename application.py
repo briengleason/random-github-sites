eeb94cc0bad80ec617308eb73424938bb600cadf
briengleason <32398682+briengleason@users.noreply.github.com>
2020-12-27 20:02:53 -0500

    Change from app to application

--- application.py
+++ application.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from query_github_sites import find_random_page
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return 'Index Page'
 
-@app.route('/api/random_site', methods=['POST'])
+@application.route('/api/random_site', methods=['POST'])
 def get_random_site():
     return find_random_page()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
